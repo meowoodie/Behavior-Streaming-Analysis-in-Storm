a2511c7a633493063c7969e2b06f9844de68d6a1
meowoodie <woodielove@163.com>
2015-10-10 15:47:13 +0800

    try debug in log file

--- streaming_topology/streaming_topology/feature_extractor_bolt.py
+++ streaming_topology/streaming_topology/feature_extractor_bolt.py
@@ -6,21 +6,21 @@ from pyleus.storm import SimpleBolt
 
 log = logging.getLogger('counter')
 
-# Counter = namedtuple("Counter", "word count")
+FEATURE = namedtuple("userId", "type", "timestamp")
 
 
 class FeatureExtractorBolt(SimpleBolt):
 
-    # OUTPUT_FIELDS = Counter
+    OUTPUT_FIELDS = FEATURE
 
     def initialize(self):
-        # self.words = defaultdict(int)
-        pass
+        self.words = defaultdict(int)
 
     def process_tuple(self, tup):
         _user_id, _type, _behavior = tup.values
-        print _user_id, _type, _behavior
-
+        timestamp = _behavior["timestamp"]
+        # print _user_id, _type, _behavior
+        log.debug("{0} {1}".format(_user_id, _type, timestamp))
         # word, = tup.values
         # self.words[word] += 1
         # log.debug("{0} {1}".format(word, self.words[word]))
@@ -30,7 +30,7 @@ class FeatureExtractorBolt(SimpleBolt):
 if __name__ == '__main__':
     logging.basicConfig(
         level=logging.DEBUG,
-        filename='/tmp/word_count_count_words.log',
+        filename='/tmp/feature_extractor_bolt.log',
         format="%(message)s",
         filemode='a',
     )
