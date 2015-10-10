cf58af72de8d6e50689adb2d82e1a414f8756ac1
meowoodie <woodielove@163.com>
2015-10-10 15:56:45 +0800

    build failed and debug for finding reason

--- streaming_topology/streaming_topology/feature_extractor_bolt.py
+++ streaming_topology/streaming_topology/feature_extractor_bolt.py
@@ -6,12 +6,12 @@ from pyleus.storm import SimpleBolt
 
 log = logging.getLogger("feature_extractor")
 
-FEATURE = namedtuple("userId", "type", "timestamp")
+# FEATURE = namedtuple("userId", "type", "timestamp")
 
 
 class FeatureExtractorBolt(SimpleBolt):
 
-    OUTPUT_FIELDS = FEATURE
+    # OUTPUT_FIELDS = FEATURE
 
     # def initialize(self):
         # self.words = defaultdict(int)
@@ -19,9 +19,9 @@ class FeatureExtractorBolt(SimpleBolt):
 
     def process_tuple(self, tup):
         _user_id, _type, _behavior = tup.values
-        timestamp = _behavior["timestamp"]
-        # print _user_id, _type, _behavior
-        log.debug(_user_id + " " + _type + " " + str(timestamp))
+        # timestamp = _behavior["timestamp"]
+        print _user_id, _type, _behavior
+        # log.debug(_user_id + " " + _type + " " + str(timestamp))
         # word, = tup.values
         # self.words[word] += 1
         # log.debug("{0} {1}".format(word, self.words[word]))
